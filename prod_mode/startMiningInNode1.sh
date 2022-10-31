c4b627d392d348425fa4e54b24df96d6c7cbe778
Sahil Rajput <sahilrajput03@gmail.com>
2022-10-31 23:29:07 +0530

    update mining script

--- prod_mode/startMiningInNode1.sh
+++ prod_mode/startMiningInNode1.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-# Unlock acc1, acc2 (acc 0 already unlocked when starting the bootNode)
-geth --exec 'personal.unlockAccount(personal.listAccounts[1], "1", 0); personal.unlockAccount(personal.listAccounts[2], "1", 0); personal.unlockAccount(personal.listAccounts[3], "1", 0)' attach bootNode/geth.ipc
 geth --exec 'miner.start()' attach node1/geth.ipc
 # src: Non-interactive Use: Script Mode: https://geth.ethereum.org/docs/interface/javascript-console
+
+# Unlock accounts manually: acc1, acc2 (acc 0 already unlocked when starting the bootNode)
+# geth --exec 'personal.unlockAccount(personal.listAccounts[1], "1", 0); personal.unlockAccount(personal.listAccounts[2], "1", 0); personal.unlockAccount(personal.listAccounts[3], "1", 0)' attach bootNode/geth.ipc
